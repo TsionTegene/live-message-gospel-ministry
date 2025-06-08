c72178a838b9c700bf8e127b42403b64e7c8423c
TsionTegene <tsionawitua@gmail.com>
2025-06-08 10:43:59 -0700

    last commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:8080',
+  origin: ['https://live-message-frontend.onrender.com/','http://localhost:8080'],
   credentials:true,
   methods: ['GET','POST','PUT','DELETE'],
 };
